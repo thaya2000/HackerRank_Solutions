0c3e459f02d6af80c139e45d5bae83bb702a78e2
thaya2000 <thevarasathayanan@gmail.com>
2024-05-01 19:49:23 +0530

    Solution AveryBigSum problem

--- Solution_in_Java/AVeryBigSum.java
+++ Solution_in_Java/AVeryBigSum.java
@@ -13,35 +13,37 @@ import java.util.stream.Stream;
 class Result {
 
     /*
-     * Complete the 'simpleArraySum' function below.
+     * Complete the 'aVeryBigSum' function below.
      *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY ar as parameter.
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
      */
 
-    public static int simpleArraySum(List<Integer> ar) {
-        int sum = 0;
-        for (int num : ar) {
-            sum += num;
+    public static long aVeryBigSum(List<Long> ar) {
+        long sum = 0;
+
+        for (int i = 0; i < ar.size(); i++) {
+            sum += ar.get(i);
         }
+
         return sum;
 
     }
 
 }
 
-public class SimpleArraySum {
+public class AVeryBigSum {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
                 .collect(toList());
 
-        int result = Result.simpleArraySum(ar);
+        long result = Result.aVeryBigSum(ar);
 
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
